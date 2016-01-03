e2291c8fefa4932bd7f17a4274166fc7f557d0fe
Evariste Boussaton <tchegito@gmail.com>
2016-01-04 00:08:03 +0100

    Fix import DB
    DB import was broken due to last commits

--- js/controllerClient.js
+++ js/controllerClient.js
@@ -81,43 +81,6 @@ app.controller("clients", function($scope, $location, $routeParams, $rootScope, 
 		$scope.client ={};
 	}
 
-	// Load a data file
-	$scope.importDb = function(file) {
-
-		console.log(file);
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			var temp = JSON.parse(e.target.result);
-			if (temp.clients && temp.contacts) {
-				alert('Chargement réussi pour:\n'+ Object.size(temp.clients)+ ' clients\n'
-				+ Object.size(temp.contacts)+' contacts\n'+
-				  Object.size(temp.factures)+' factures\n'+
-				  Object.size(temp.ndfs)+' notes de frais');
-				dbEngine.importDb(temp);
-				// Fix ndf
-/*
-				for (var prop in db.ndfs) {
-					console.log("iterate ndf");
-					var ndf = db.ndfs[prop];
-					try {
-						for (var l = 0; l < ndf.lignes.length; l++) {
-							var dat = ndf.lignes[l].dateNoteTime;
-							var formattedDat = $filter('date')(new Date(dat), 'dd/MM/yy');
-							console.log("date=" + dat + " devient " + formattedDat);
-							ndf.lignes[l].dateNote = new Date(dat).getTime();
-						}
-					} catch (e) {
-						alert(e);
-					}
-				}*/
-				// Tell all the scope to reinit
-				$rootScope.$broadcast('reinit', $scope.name);
-			}
-		};
-
-	    reader.readAsText(file);
-	}
-
 	$scope.deleteClient = function(idClient) {
 		// Consistent checked
 		var consistent = true;
--- js/controllerCompany.js
+++ js/controllerCompany.js
@@ -1,9 +1,7 @@
-// ContrÙleur de l'onglet "DonnÈes": essentiellement les attributs de l'entreprise gÈrÈe
-app.controller("company", function($scope, $location, $routeParams, $rootScope) {
+// Contr√¥leur de l'onglet "Donn√©es": essentiellement les attributs de l'entreprise g√©r√©e
+app.controller("company", function($scope, $location, $rootScope) {
 
     var fields = ["nom", "adresse1", "adresse2", "adresse3", "statut", "siret", "rcs", "tvaIntra", "urlImage"];
-
-    $scope.c = angular.copy(db.company);
 
     $scope.listFields = function() {
         return fields;
@@ -17,10 +15,56 @@ app.controller("company", function($scope, $location, $routeParams, $rootScope) 
 
     }
 
+    $scope.$on('reinit', function(event, args) {
+        init();
+    });
+
+    // Load a data file
+    $scope.importDb = function(file) {
+
+        console.log(file);
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var temp = JSON.parse(e.target.result);
+            if (temp.clients && temp.contacts) {
+                alert('Chargement r√©ussi pour:\n'+ Object.size(temp.clients)+ ' clients\n'
+                    + Object.size(temp.contacts)+' contacts\n'+
+                    Object.size(temp.factures)+' factures\n'+
+                    Object.size(temp.ndfs)+' notes de frais');
+                dbEngine.importDb(temp);
+                // Fix ndf
+                /*
+                 for (var prop in db.ndfs) {
+                 console.log("iterate ndf");
+                 var ndf = db.ndfs[prop];
+                 try {
+                 for (var l = 0; l < ndf.lignes.length; l++) {
+                 var dat = ndf.lignes[l].dateNoteTime;
+                 var formattedDat = $filter('date')(new Date(dat), 'dd/MM/yy');
+                 console.log("date=" + dat + " devient " + formattedDat);
+                 ndf.lignes[l].dateNote = new Date(dat).getTime();
+                 }
+                 } catch (e) {
+                 alert(e);
+                 }
+                 }*/
+                // Tell all the scope to reinit
+                $rootScope.$broadcast('reinit', $scope.name);
+                // Refresh current form
+                $scope.$apply();
+            }
+        };
+
+        reader.readAsText(file);
+    }
+
     function init() {
+        console.log('reinit company controller');
+
         for (i in fields) {
             var fieldName = fields[i];
             $scope.formData[fieldName] = db.company[fieldName];
         }
+        $scope.c = angular.copy(db.company);
     }
 });
